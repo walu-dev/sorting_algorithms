efc3cb2165f460d942d149ebc031c9e77934d58a
walu-dev <jonathanenoch11@gmail.com>
2023-04-28 17:49:20 +0000

    fin

---
+++ 1000-sort_deck.c
@@ -0,0 +1,30 @@
+#include "deck.h"
+
+int _strcmp(const char *s1, const char *s2);
+char get_value(deck_node_t *card);
+void insertion_sort_deck_kind(deck_node_t **deck);
+void insertion_sort_deck_value(deck_node_t **deck);
+void sort_deck(deck_node_t **deck);
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
+ *
+ * Return: Positive byte difference if s1 > s2
+ *         0 if s1 == s2
+ *         Negative byte difference if s1 < s2
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+while (*s1 && *s2 && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+
+if (*s1 != *s2)
+return (*s1 - *s2);
+return (0);
+}
+
